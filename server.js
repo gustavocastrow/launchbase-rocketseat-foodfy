1b2fccaa7dac10faf203cadd8c15a806ddaa5230
gustavocastro <gustavocastrocs@gmail.com>
2020-02-04 06:19:40 -0300

    Configurando servidor

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+
+server.set('view engine', 'html');
+
+nunjucks.configure('views', {
+  express:server 
+});
+
+
+server.get('/', function(req, res){
+  return res.render('index')
+
+
+})
+
+server.listen(3000, function(){
+  console.log('Server is running')
+})
